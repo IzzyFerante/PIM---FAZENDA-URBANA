f2c6262ee1d118ae02a65ed9afb9b93569a590fc
Isabelle Ferante <bebelferante@gmail.com>
2024-05-06 12:56:45 -0300

    adicionei o Metro Modern Ui  (pacote interface)

---
+++ Programa PIM/DB/DBConnection.cs
@@ -0,0 +1,26 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class DBConnection : IDisposable
+    {
+        public NpgsqlConnection Connection { get; set; }
+
+        public DBConnection()
+        {
+            Connection = new NpgsqlConnection("Server=***********;Port=5432;Database=fazenda_urbana;User Id=postgres;Password=;");
+            Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            Connection.Dispose();
+        }
+
+    }
+}
---
+++ Programa PIM/DB/ListarTabela.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// FAZER ISSO PARA TODAS AS TABELAS
+
+namespace DB
+{
+    public class ListarTabela
+    {
+        public bool Insert (/*objeto="tabela"*/ /*nome objeto*/)
+        {
+            using var conn = new DBConnection();
+
+            string query = @"";
+
+            var result = conn.Connection.Execute(sql: query, param: /*nome do objeto acima*/);
+
+            return result == 1;
+        }
+
+        public List</*nome tabela*/> Get()
+        {
+            using var conn = new DBConnection();
+
+            string query = @"SELECT * FROM /*nome tabela*/;";
+
+            var /*nome objeto*/ = conn.Connection.Query</*nome tabela*/>(sql: query);
+
+            return /*nome var acima*/.ToList();
+        }
+
+
+    }
+}
